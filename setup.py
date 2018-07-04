5184233b6e9ea8467142a8d26bacf7a8e5869edc
Poli García <poli@devartis.com>
2018-07-03 23:49:44 -0300

    Update setup.py
    Update method to retrieve dependencies based on https://stackoverflow.com/a/49837302
    Update version number to dev

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ import os
 from os.path import dirname
 from os.path import join
 
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 from setuptools import setup
 
-__version__ = '0.1.7'
+__version__ = '0.1.8dev'
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
